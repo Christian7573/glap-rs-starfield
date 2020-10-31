756b62585ba4a88e3a627ff9d0008f34c7fb70ce
Christian7573 <cag4561@gmail.com>
2020-10-31 14:28:09 -0500

    Some sketching of how I think it will work

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,22 @@ export class Starfield {
 }
 
 export class StarfieldChunk {
-	stars: Star[];
-	nebula_fogs: NebulaFog[];
+	stars: Star[] = [];
+	nebula_fogs: NebulaFog[] = [];
 }
 
 export class Star {
-	x: number;
-	y: number;
-	size: number;
-	color: number;
+	readonly x: number;
+	readonly y: number;
+	readonly size: number;
+	readonly color: number;
+
+	constructor(x: number, y: number, size: number, color: number) {
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		this.color = color;
+	}
 }
 export class NebulaFog {
 	x: number;
@@ -30,5 +37,38 @@ export enum StarType {
 }
 
 export class StarfieldChunkGenerator {
+	tophat_threshold = 0.95;
+	small_star_threshold = 0.8;
+	medium_star_threshold = 0.85;
+	large_star_threshold = 0.9;
 
+	small_star_size = 3;
+	medium_star_size = 8;
+	large_star_size = 15;
+
+	generate(seed_x: string, seed_y: string, size: number) {
+		const chunk = new StarfieldChunk();
+
+		//Small stars
+		for (const [x, y] of this.points_above_threshold(new RandomContainer(seed_x + "_s", seed_y + "_s"), size, this.small_star_threshold)) {
+			chunk.stars.push(new Star(x, y, this.small_star_size, 0xffffff));
+		}
+
+		//Medium stars
+		for (const [x, y] of this.points_above_threshold(new RandomContainer(seed_x + "_m", seed_y + "_m"), size, this.medium_star_threshold)) {
+			chunk.stars.push(new Star(x, y, this.medium_star_size, 0xffffff));
+		}
+
+		//Lage stars
+		for (const [x, y] of this.points_above_threshold(new RandomContainer(seed_x + "_l", seed_y + "_l"), size, this.large_star_threshold)) {
+			chunk.stars.push(new Star(x, y, this.large_star_size, 0xffffff));
+		}
+	}
+	points_above_threshold(random: RandomContainer, size: number, threshold: number): [number, number][] {
+		const out = [];
+		for (const position of random.iter(size, size)) {
+			if (position.vx >= threshold && position.vy >= threshold) out.push([position.x, position.y]);
+		}
+		return out;
+	}
 }
