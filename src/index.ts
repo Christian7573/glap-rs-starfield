91427621f9fc2bc11caddd86c95f27e1be46170e
Christian7573 <cag4561@gmail.com>
2020-11-05 20:56:38 -0600

    Added layering, bit of refractoring, started SVG renderer

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,100 @@ import Type_SvgRenderer from "./svg";
 
 export async function SvgRenderer(): Promise<typeof Type_SvgRenderer> { return (await import("./svg")).default; }
 
-export class Starfield {
+export class GlapRsStarfield {
+	foreground_layer: StarfieldLayer;
+	middle_layer: StarfieldLayer;
+	background_layer: StarfieldLayer;
+	chunk_size = 1000;
+
+	constructor(seed: string) {
+		this.foreground_layer = new StarfieldLayer(seed + "_f", StarfieldChunkGenerator.foreground_layer(), this.chunk_size, 5);
+		this.middle_layer = new StarfieldLayer(seed + "_m", StarfieldChunkGenerator.middle_layer(), this.chunk_size * 2, 4);
+		this.background_layer = new StarfieldLayer(seed + "_b", StarfieldChunkGenerator.background_layer(), this.chunk_size * 4, 4);
+	}
+
+	viewpoints = [
+		new GlapRsViewspot(this, -100, -100, this.chunk_size),
+		new GlapRsViewspot(this,  100, -100, this.chunk_size),
+		new GlapRsViewspot(this, -100,  100, this.chunk_size),
+		new GlapRsViewspot(this,  100,  100, this.chunk_size)
+	];
+	update_player_position(x: number, y: number) {
+		for (const viewpoint of this.viewpoints) viewpoint.update_player_position(x, y);
+	}
+}
+export default GlapRsStarfield;
+
+class GlapRsViewspot {
+	starfield: GlapRsStarfield;
+	offset_x: number;
+	offset_y: number;
+	last_chunk_x = 100000;
+	last_chunk_y = 100000;
+	chunk_size: number;
+
+	constructor(starfield: GlapRsStarfield, offset_x: number, offset_y: number, chunk_size: number) {
+		this.starfield = starfield;
+		this.offset_x = offset_x;
+		this.offset_y = offset_y;
+		this.chunk_size = chunk_size;
+	}
+	update_player_position(player_x: number, player_y: number) {
+		const x = player_x + this.offset_x;
+		const y = player_y + this.offset_y;
+		const chunk_x = Math.floor(x / this.chunk_size);
+		const chunk_y = Math.floor(y / this.chunk_size);
+		if (chunk_x !== this.last_chunk_x || chunk_y !== this.last_chunk_y) {
+			this.last_chunk_x = chunk_x;
+			this.last_chunk_y = chunk_y;
+			this.starfield.foreground_layer.ensure_chunk(chunk_x, chunk_y);
+			this.starfield.middle_layer.ensure_chunk(Math.floor(chunk_x / 2), Math.floor(chunk_y / 2));
+			this.starfield.background_layer.ensure_chunk(Math.floor(chunk_x / 4), Math.floor(chunk_y / 4));
+		}
+	}
+
+}
+
+export class StarfieldLayer {
 	chunks = new Map<string, StarfieldChunk>();
-	chunks_ordered: Starfield[] = [];
+	chunks_ordered: string[] = [];
 	chunk_size: number;
 	max_chunks: number;
 	seed: string;
+	generator: StarfieldChunkGenerator;
 
-	constructor(seed: string, chunk_size: number, max_chunks: number) {
-		this.seed = seed;
+	constructor(seed: string, generator: StarfieldChunkGenerator, chunk_size: number, max_chunks: number) {
+		this.generator = generator;
 		this.chunk_size = chunk_size;
 		this.max_chunks = max_chunks;
 	}
-	
+
+	ensure_chunk(chunk_x: number, chunk_y: number) {
+		const addr = `${chunk_x},${chunk_y}`;
+		const chunk = this.chunks.get(addr);
+		if (chunk === null) {
+			const chunk = this.generator.generate(`${addr}_${chunk_x}`, `${addr}_${chunk_y}`, 1000);
+			chunk.x = chunk_x;
+			chunk.y = chunk_y;
+			this.chunks.set(addr, chunk);
+			this.chunks_ordered.push(addr);
+			if (this.chunks_ordered.length > this.max_chunks) {
+				const removed_chunk = this.chunks_ordered[0];
+				this.chunks_ordered.splice(0,1);
+				this.chunks.delete(removed_chunk);
+			}
+		} else {
+			this.chunks_ordered.splice(this.chunks_ordered.indexOf(addr), 1);
+			this.chunks_ordered.push(addr);
+		}
+	}
 }
 
 export class StarfieldChunk {
 	stars: Star[] = [];
 	nebula_fogs: NebulaFog[] = [];
+	x: number;
+	y: number;
 }
 
 export class Star {
@@ -50,17 +126,23 @@ export enum StarType {
 	OldLogo,
 }
 
+
 export class StarfieldChunkGenerator {
-	tophat_threshold = 0.95;
+	/*tophat_threshold = 0.95;
 	small_star_threshold = 0.8;
 	medium_star_threshold = 0.85;
-	large_star_threshold = 0.9;
+	large_star_threshold = 0.9;*/
+	
+   	tophat_threshold = 1;
+	small_star_threshold = 1;
+	medium_star_threshold = 1;
+	large_star_threshold = 1;
 
 	small_star_size = 3;
 	medium_star_size = 8;
 	large_star_size = 15;
 
-	generate(seed_x: string, seed_y: string, size: number) {
+	generate(seed_x: string, seed_y: string, size: number): StarfieldChunk {
 		const chunk = new StarfieldChunk();
 
 		//Small stars
@@ -77,12 +159,31 @@ export class StarfieldChunkGenerator {
 		for (const [x, y] of this.points_above_threshold(new RandomContainer(seed_x + "_l", seed_y + "_l"), size, this.large_star_threshold)) {
 			chunk.stars.push(new Star(x, y, this.large_star_size, 0xffffff));
 		}
+		
+		return chunk;
 	}
+
 	points_above_threshold(random: RandomContainer, size: number, threshold: number): [number, number][] {
 		const out = [];
 		for (const position of random.iter(size, size)) {
 			if (position.vx >= threshold && position.vy >= threshold) out.push([position.x, position.y]);
 		}
 		return out;
+	}
+
+	static foreground_layer(): StarfieldChunkGenerator {
+		const gen = new StarfieldChunkGenerator();
+		gen.small_star_threshold = 0.8;
+		return gen;
+	}
+	static middle_layer(): StarfieldChunkGenerator {
+		const gen = new StarfieldChunkGenerator();
+		gen.medium_star_threshold = 0.85;
+		return gen;
+	}
+	static background_layer(): StarfieldChunkGenerator {
+		const gen = new StarfieldChunkGenerator();
+		gen.large_star_threshold = 0.9;
+		return gen;
 	}
 }
--- src/svg.ts
+++ src/svg.ts
@@ -1,6 +1,42 @@
-import { StarfieldChunk } from "./index";
-
+import { GlapRsStarfield, StarfieldChunk, Star, StarType } from "./index";
 
 export default class SvgRenderer {
+	starfield: GlapRsStarfield;
+	rendered_chunks: Map<StarfieldChunk, SVGElement> = new Map();
+	root: SVGElement;
+	foreground_layer: SVGGElement;
+	middle_layer: SVGGElement;
+	background_layer: SVGGElement;
+	
+	constructor(starfield: GlapRsStarfield, render_inside: SVGElement) {
+		this.starfield = starfield;
+		this.root = render_inside;
+		this.foreground_layer = document.createElementNS("http://www.w3.org/2000/svg", "g");
+		this.middle_layer = document.createElementNS("http://www.w3.org/2000/svg", "g");
+		this.background_layer = document.createElementNS("http://www.w3.org/2000/svg", "g");
+	}
 
+	update_player_position(x: number, y: number) {
+		this.foreground_layer.style.transform = `translate(${-x}px, ${y}px)`;
+		this.middle_layer.style.transform = `translate(${-x / 2}px, ${-y / 2}px)`;
+		this.background_layer.style.transform = `translate(${-x / 4}px, ${-y / 4}px)`;
+	}
+
+	update_chunks() {
+		const to_delete = [];
+		for (const chunk of this.rendered_chunks.keys()) to_delete.push(chunk);
+		for (const chunk of this.starfield.foreground_layer.chunks.values()) {
+			const i = to_delete.indexOf(chunk);
+			if (i < 0) {
+				const rendered_chunk = this.render_chunk(chunk);
+
+			} else {
+				to_delete.splice(i, 1);
+			}
+		}
+	}
+
+	render_chunk(chunk: StarfieldChunk) {
+
+	}
 }
