cba0803a6c69ea6a6ff1333db467df4d0da6006b
Christian7573 <cag4561@gmail.com>
2021-02-04 16:35:30 -0600

    Newness

--- src/index.ts
+++ src/index.ts
@@ -4,143 +4,47 @@ export { default as SvgRenderer } from "./svg";
 
 type MyRandom = seedrandom.prng;
 
-export class GlapRsStarfield {
-	foreground_layer: StarfieldLayer;
-	middle_layer: StarfieldLayer;
-	background_layer: StarfieldLayer;
-	chunk_size = 200;
-
-	constructor(seed: string) {
-		this.foreground_layer = new StarfieldLayer(seed + "_f", StarfieldChunkGenerator.foreground_layer(), this.chunk_size, 12);
-		this.middle_layer = new StarfieldLayer(seed + "_m", StarfieldChunkGenerator.middle_layer(), this.chunk_size * 2, 8);
-		this.background_layer = new StarfieldLayer(seed + "_b", StarfieldChunkGenerator.background_layer(), this.chunk_size * 4, 4);
-	}
-
-	viewpoints = [
-		new GlapRsViewspot(this, -100, -100, this.chunk_size),
-		new GlapRsViewspot(this,  100, -100, this.chunk_size),
-		new GlapRsViewspot(this, -100,  100, this.chunk_size),
-		new GlapRsViewspot(this,  100,  100, this.chunk_size)
-	];
-	update_player_position(x: number, y: number): boolean {
-		let return_val = false;
-		for (const viewpoint of this.viewpoints) return_val = viewpoint.update_player_position(x, y) || return_val;
-		return return_val;
-	}
-}
-export default GlapRsStarfield;
-
-class GlapRsViewspot {
-	starfield: GlapRsStarfield;
-	offset_x: number;
-	offset_y: number;
-	last_chunk_x = 100000;
-	last_chunk_y = 100000;
-	chunk_size: number;
-
-	constructor(starfield: GlapRsStarfield, offset_x: number, offset_y: number, chunk_size: number) {
-		this.starfield = starfield;
-		this.offset_x = offset_x;
-		this.offset_y = offset_y;
-		this.chunk_size = chunk_size;
-	}
-	update_player_position(player_x: number, player_y: number): boolean {
-		const x = player_x + this.offset_x;
-		const y = player_y + this.offset_y;
-		const chunk_x = Math.floor(x / this.chunk_size);
-		const chunk_y = Math.floor(y / this.chunk_size);
-		if (chunk_x !== this.last_chunk_x || chunk_y !== this.last_chunk_y) {
-			this.last_chunk_x = chunk_x;
-			this.last_chunk_y = chunk_y;
-			this.starfield.foreground_layer.ensure_chunk(chunk_x, chunk_y);
-			this.starfield.middle_layer.ensure_chunk(Math.floor(chunk_x / 2), Math.floor(chunk_y / 2));
-			this.starfield.background_layer.ensure_chunk(Math.floor(chunk_x / 4), Math.floor(chunk_y / 4));
-			return true;
-		}
-		return false;
-	}
-
+enum ObjKind {
+	Square,
+	Triangle,
+	WackTriangle,
+	NebulaCloud,
 }
 
-export class StarfieldLayer {
-	chunks = new Map<string, StarfieldChunk>();
-	chunks_ordered: string[] = [];
-	chunk_size: number;
-	max_chunks: number;
-	seed: string;
-	generator: StarfieldChunkGenerator;
+type OutputFunc = (kind: ObjKind, x: number, y: number, rot: number, size: number, color: number) => void;
 
-	constructor(seed: string, generator: StarfieldChunkGenerator, chunk_size: number, max_chunks: number) {
-		this.seed = seed;
-		this.generator = generator;
-		this.chunk_size = chunk_size;
-		this.max_chunks = max_chunks;
-	}
-
-	ensure_chunk(chunk_x: number, chunk_y: number) {
-		const addr = `${chunk_x},${chunk_y}`;
-		const chunk = this.chunks.get(addr);
-		if (chunk == null) {
-			const chunk = this.generator.generate(seedrandom(`{this.seed}_{chunk_x}_{chunk_y}`), this.chunk_size);
-			chunk.x = chunk_x;
-			chunk.y = chunk_y;
-			this.chunks.set(addr, chunk);
-			this.chunks_ordered.push(addr);
-			if (this.chunks_ordered.length > this.max_chunks) {
-				const removed_chunk = this.chunks_ordered[0];
-				this.chunks_ordered.splice(0,1);
-				this.chunks.delete(removed_chunk);
-			}
-		} else {
-			this.chunks_ordered.splice(this.chunks_ordered.indexOf(addr), 1);
-			this.chunks_ordered.push(addr);
-		}
+export function populate_with_randomly(rng: MyRandom, kinds: ObjKind[], count: [number, number], sizes: [number, number], colors: number[], max_pos: number, output: OutputFunc) {
+	const objects = ((count[1] - count[0]) * rng()) + count[0];
+	for (let _i = 0; _i < objects; _i++) {
+		const x = rng() * max_pos;
+		const y = rng() * max_pos;
+		const kind = kinds[Math.floor(rng() * kinds.length)];
+		const size = ((sizes[1] - sizes[0]) * rng()) + sizes[0];
+		const color = colors[Math.floor(rng() * colors.length)];
+		const rot = rng() * Math.PI * 2;
+		output(kind, x, y, rot, size, color);
 	}
 }
 
-export class StarfieldChunk {
-	stars: Star[] = [];
-	nebula_fogs: NebulaFog[] = [];
-	x: number;
-	y: number;
-}
+export function populate_with_branch(rng: MyRandom, kinds: ObjKind[], branch_continuity: number, continuity_decrease: number, base_pos: [number, number], offset: [number, number], rot_range: [number, number], colors: number[], sizes: [number, number], output: OutputFunc) {
+	const kind = kinds[Math.floor(rng() * kinds.length)];
+	const size = ((sizes[1] - sizes[0]) * rng()) + sizes[0];
+	const color = colors[Math.floor(rng() * colors.length)];
+	const my_rot = rng() * Math.PI * 2;
+	output(kind, base_pos[0], base_pos[1], my_rot, size, color);
 
-export class Star {
-	readonly x: number;
-	readonly y: number;
-	readonly size: number;
-	readonly color: number;
-	readonly kind: StarKind;
-
-	constructor(x: number, y: number, size: number, color: number, kind: StarKind) {
-		this.x = x;
-		this.y = y;
-		this.size = size;
-		this.color = color;
-		this.kind = kind;
+	while (rng() <= branch_continuity) {
+		branch_continuity -= continuity_decrease;
+		const length = ((offset[1] - offset[0]) * rng()) + offset[0];
+		const rot = ((rot_range[1] - rot_range[0]) * rng()) + rot_range[0];
+		const x = Math.cos(rot) * length;
+		const y = Math.sin(rot) * length;
+		const new_rots: [number, number] = [rot - ((rot_range[1] - rot_range[0]) * 0.25), rot + ((rot_range[1] - rot_range[0]) * 0.25)];
+		populate_with_branch(rng, kinds, branch_continuity, continuity_decrease, [base_pos[0] + x, base_pos[1] + y], offset, new_rots, colors, sizes, output);
 	}
 }
-export class NebulaFog {
-	x: number;
-	y: number;
-	radians: number;
-	size: number;
-	color: number;
-	opacity: number;
-}
-export enum StarKind {
-	Star,
-	TopHat, //Easter eggs go brrrrrr
-	OldLogo,
-}
-
 
 export class StarfieldChunkGenerator {
-	/*tophat_threshold = 0.95;
-	small_star_threshold = 0.8;
-	medium_star_threshold = 0.85;
-	large_star_threshold = 0.9;*/
-	
    	max_tophats = 0;
 	max_small_stars = 0;
 	max_medium_stars = 0;
@@ -149,18 +53,11 @@ export class StarfieldChunkGenerator {
 	forced_medium_stars = 0;
 	forced_large_stars = 0;
 
-	/*small_star_size = 3 / 30;
-	medium_star_size = 8 / 30;
-	large_star_size = 15 / 30;*/
 	small_star_size = 8 / 30;
 	medium_star_size = 10 / 30;
 	large_star_size = 15 / 30;
-   	/*small_star_size = 10;
-	medium_star_size = 10;
-	large_star_size = 10;*/
 
 	random: MyRandom;
-	chunk: StarfieldChunk;
 	size: number;
 	max_skip = 5;
 
